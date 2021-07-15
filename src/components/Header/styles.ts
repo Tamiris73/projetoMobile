2c6a87cb811ce92238a59ff20c9a4244a6d02c2d
Tamiris <btamiris73@gmail.com>
2021-07-15 19:40:47 -0300

    codigo parcial

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components/native'
+import colors from '../../styles/colors'
+
+export const HeaderStyle = styled.View`
+  flex-direction: row;
+`
+export const HeaderTitle = styled.View`
+  width: 50%;
+  margin-left: 20px;
+  justify-content: center;
+`
+export const HeaderImage = styled.View`
+  width: 50%;
+  align-items: center;
+`
+export const HeaderHello = styled.Text`
+  font-size: 18px;
+`
+export const HeaderName = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+`
---
+++ src/styles/colors.ts
@@ -0,0 +1,8 @@
+export default {
+  black: '#000000',
+  white: '#FFFFFF',
+  gray: '#F4F4F4',
+  blue: '#413F92',
+  red: '#C82929', 
+  green: '#3CB371'
+}
